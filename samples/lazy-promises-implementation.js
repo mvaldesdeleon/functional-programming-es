1bbdd37210ce975c28ec2d975f3f3b47a5ee7d62
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2016-09-13 20:36:12 -0300

    Adds type signatures to lazy promises implementation code sample.

--- samples/lazy-promises-implementation.js
+++ samples/lazy-promises-implementation.js
@@ -1,22 +1,37 @@
 function Lazy(x) { this.__value = x; };
 
+// unit :: (a -> Promise[b,x]) -> Lazy[a -> Promise[b,x]]
 Lazy.of = x => new Lazy(x);
 
+// map :: Lazy[a -> Promise[b,x]] -> (b -> c) -> Lazy[a -> Promise[c,x]]
+// map :: Lazy[a -> Promise[b,x]] -> (b -> Promise[c,y]) -> Lazy[a -> Promise[c,y]]
 Lazy.prototype.map = function(fn) {
-    return Lazy.of(() => this.__value().then(fn));
+    return Lazy.of((x) => this.__value(x).then(fn));
 };
 
-Lazy.prototype.do = function() {
-    return this.__value();
+// do :: Lazy[a -> Promise[b,z]] -> a -> Promise[b,z]
+Lazy.prototype.do = function(x) {
+    return this.__value(x);
 };
 
-function thunk() {
-    return new Promise((res, rej) => setTimeout(res.bind(null, 100), 3000));
+// thunk :: _ -> Promise[Number, a]               Resolved[Number]
+function thunk(n) { //                               /
+    return new Promise((res, rej) => setTimeout(res.bind(null, 100 * n), 1500 * n));
 };
 
-var l = Lazy.of(thunk).map(x => x + 50).map(x => x * 2);
+// addFifty :: Number -> Number
+var addFifty = x => x + 50
 
-l.do().then(console.log.bind(console, 'res'));
+// timesTwoDelayed :: Number -> Promise[Number, a]
+var timesTwoDelayed = x => new Promise((res, rej) => setTimeout(res.bind(null, x * 2), 1000));
+//                                                                 \
+//                                                               Resolved[Number]
+
+var lazy = Lazy.of(thunk).map(addFifty).map(timesTwoDelayed);
+
+lazy.do(1).then(console.log.bind(console, 'res'));
 // res 300
-l.do().then(console.log.bind(console, 'res'));
-// res 300
+lazy.do(2).then(console.log.bind(console, 'res'));
+// res 500
+lazy.do(3).then(console.log.bind(console, 'res'));
+// res 700
