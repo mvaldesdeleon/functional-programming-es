eb8e43dee0e2bbdbf0014cbae2210889fd648968
Martín Valdés de León <m.valdesdeleon@sovanta.com>
2016-09-13 20:59:48 -0300

    Adds type signatures to synchronous control flow implementation code sample.

--- samples/synchronous-control-flow-implementation.js
+++ samples/synchronous-control-flow-implementation.js
@@ -1,3 +1,5 @@
+// Either[a, b] = Left[a] ㅣ Right[b]
+
 function Either(x) { this.__value = x; };
 
 function Left(...args) { Either.call(this, ...args); };
@@ -6,7 +8,9 @@ Left.prototype = Object.create(Either.prototype);
 function Right(...args) { Either.call(this, ...args); };
 Right.prototype = Object.create(Either.prototype);
 
+// ʟeft :: a -> Left[a]
 Left.of = x => new Left(x);
+// ʀight :: b -> Right[b]
 Right.of = x => new Right(x);
 
 Left.of(0) instanceof Left && Left.of(0) instanceof Either && !(Left.of(0) instanceof Right)
@@ -14,6 +18,7 @@ Left.of(0) instanceof Left && Left.of(0) instanceof Either && !(Left.of(0) insta
 Right.of(0) instanceof Right && Right.of(0) instanceof Either && !(Right.of(0) instanceof Left)
 // true
 
+// map :: Either[a, x] -> (a -> b) -> Either[b, x]
 Either.prototype.map = function(fn) {
     if (this instanceof Right)
         return Right.of(fn(this.__value));
@@ -21,55 +26,78 @@ Either.prototype.map = function(fn) {
         return this;
 };
 
-Right.of(0).map(x => x + 1).map(x => x * 2).map(x => x.toString());
+// plusOne :: Number -> Number
+var plusOne = x => x + 1
+
+// timesTwo :: Number -> Number
+var timesTwo = x => x * 2
+
+// toString :: Number -> String
+var toString = x => x.toString();
+
+Right.of(0).map(plusOne).map(timesTwo).map(toString);
 // Right {__value: "2"}
 
+// divideTwoBy :: Number -> Eiter[Number, String]
 var divideTwoBy = function(x) {
     if (x === 0) return Left.of("can't divide by zero");
     else return Right.of(2 / x);
 };
 
-Right.of(1).map(x => x - 1).map(divideTwoBy).map(console.log.bind(console));
+// subtractOne :: Number -> Number
+var subtractOne = x => x - 1
+
+// subtractTwo :: Number -> Number
+var subtractTwo = x => x - 2
+
+Right.of(1).map(subtractOne).map(divideTwoBy).map(console.log.bind(console));
 // Left {__value: "can't divide by zero"}
 // Right {__value: undefined}
 
-Right.of(1).map(x => x - 1).map(divideTwoBy);
+Right.of(1).map(subtractOne).map(divideTwoBy);
 // Right {__value: Left}
 
+// join :: Either[Either[a, x], y] -> Either[a, x]
 Either.prototype.join = function() {
     return this.__value;
 };
 
-Right.of(1).map(x => x - 1).map(divideTwoBy).join().map(x => x + 10).map(console.log.bind(console));
+// addTen :: Number -> Number
+var addTen = x => x + 10
+
+Right.of(1).map(subtractOne).map(divideTwoBy).join().map(addTen).map(console.log.bind(console));
 // Left {__value: "can't divide by zero"}
 
-Right.of(1).map(x => x - 2).map(divideTwoBy).join().map(x => x + 10).map(console.log.bind(console));
+Right.of(1).map(subtractTwo).map(divideTwoBy).join().map(addTen).map(console.log.bind(console));
 // 8
 // Right {__value: undefined}
 
+// chain :: Either[a, x] -> (a -> Either[b, y]) -> Either[b, y]
 Either.prototype.chain = function(fn) {
     return this.map(fn).join();
 };
 
-Right.of(1).map(x => x - 2).chain(divideTwoBy).map(x => x + 10).map(console.log.bind(console));
+Right.of(1).map(subtractTwo).chain(divideTwoBy).map(addTen).map(console.log.bind(console));
 // 8
 // Right {__value: undefined}
 
-Right.of(1).map(x => x - 1).chain(divideTwoBy).map(x => x + 10).map(console.log.bind(console));
+Right.of(1).map(subtractOne).chain(divideTwoBy).map(addTen).map(console.log.bind(console));
 // Left {__value: "can't divide by zero"}
 
-Right.of(1).map(x => x - 2).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
+Right.of(1).map(subtractTwo).chain(divideTwoBy).map(addTen).map(toString);
 // Right {__value: "8"}
-Right.of(2).map(x => x - 2).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
+Right.of(2).map(subtractTwo).chain(divideTwoBy).map(addTen).map(toString);
 // Left {__value: "can't divide by zero"}
 
+// unit :: b -> Right[b]
 Either.of = x => new Right(x);
 
-Either.of(1).map(x => x - 2).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
+Either.of(1).map(subtractTwo).chain(divideTwoBy).map(addTen).map(toString);
 // Right {__value: "8"}
-Either.of(1).map(x => x - 1).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
+Either.of(1).map(subtractOne).chain(divideTwoBy).map(addTen).map(toString);
 // Left {__value: "can't divide by zero"}
 
+// do :: Either[a, b] -> (a -> x) -> (b -> x) -> x
 Either.prototype.do = function(l, r) {
     if (this instanceof Right)
         return r(this.__value);
@@ -77,11 +105,12 @@ Either.prototype.do = function(l, r) {
         return l(this.__value);
 };
 
-var either => (l, r) = e => e instanceof Left ? l(e.__value) : r(e.__value);
+// either :: [(a -> x), (b -> x)] -> Either[a, b] -> x
+var either = (l, r) => e => e instanceof Left ? l(e.__value) : r(e.__value);
 
-var a = Either.of(1).map(x => x - 1).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
-var b = Either.of(1).map(x => x - 2).chain(divideTwoBy).map(x => x + 10).map(x => x.toString());
-var e = either(console.log.bind(console,'this is a left!'), console.log.bind(console, 'this is a right!');
+var a = Either.of(1).map(subtractOne).chain(divideTwoBy).map(addTen).map(toString);
+var b = Either.of(1).map(subtractTwo).chain(divideTwoBy).map(addTen).map(toString);
+var e = either(console.log.bind(console,'this is a left!'), console.log.bind(console, 'this is a right!'));
 
 e(a);
 // this is a left! can't divide by zero
